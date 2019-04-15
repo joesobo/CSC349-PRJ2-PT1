360ad37996e4bc67505ba07234edbdb16b427247
joesobo <soboleskijoe@gmail.com>
2019-04-15 08:56:39 -0700

    added initial matrixProduct method

--- main.java
+++ main.java
@@ -1,9 +1,30 @@
-public class main{
-    public static void matrixProduct(){
+import java.util.Arrays;
 
+public class main{
+    public static int[][] matrixProduct(int[][] A, int[][] B){
+        int m = b[0].length; //col
+        int n = a.length; //row
+        int k;
+        int[][] arr;
+
+        if(b.length == a[0].length){
+            k = b.length;
+        }else{
+            throw(IllegalArgumentException);
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                for(int l = 0; l < k; l++){
+                    arr[i][j] = A[i][l] * B[l][j];
+                }
+            }
+        }
+
+        return arr;
     }
 
     public static void main(String args[]){
-        
+
     }
 }
